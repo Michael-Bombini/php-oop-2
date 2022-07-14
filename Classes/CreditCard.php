82d138ed6e2160df78618409a32b873668a7384e
Michael Bombini <bombini.michael@gmail.com>
2022-07-14 15:52:22 +0200

    Added user and card Classes

--- Classes/CreditCard.php
+++ Classes/CreditCard.php
@@ -1,5 +1,90 @@
 <?php 
 
 class CreditCard{
+    private $cardNumber;
+    private $cvv;
+    private $expiryDate;
+
+
+    function __construct($_cardNumber , $_cvv , $_expiryDate)
+    {
+        $this->setCardNumber($_cardNumber);
+        $this->setCvv($_cvv);
+        $this->setExpiryDate($_expiryDate);
+    }
+
+
+    /**
+     * Get the value of cardNumber
+     */ 
+    protected function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * Set the value of cardNumber
+     *
+     * @return  self
+     */ 
+    protected function setCardNumber($cardNumber)
+    {
+        $this->cardNumber = $cardNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cvv
+     */ 
+    protected function getCvv()
+    {
+        return $this->cvv;
+    }
+
+    /**
+     * Set the value of cvv
+     *
+     * @return  self
+     */ 
+    protected function setCvv($cvv)
+    {
+        $this->cvv = $cvv;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of expiryDate
+     */ 
+    protected function getExpiryDate()
+    {
+        return $this->expiryDate;
+    }
+
+    /**
+     * Set the value of expiryDate
+     *
+     * @return  self
+     */ 
+    protected function setExpiryDate($expiryDate)
+    {
+        $this->expiryDate = $expiryDate;
+
+        return $this;
+    }
+
+
+    protected function isExpired(){
+      $expire = $this->getExpiryDate;
+      $expire = strtotime($expire);
+      $todayMonthYear = date("m") + date("y");
+      if($expire> $todayMonthYear){
+        return ;
+      }
+      
+    }
+
+
     
 }
---
+++ Classes/User.php
@@ -0,0 +1,124 @@
+<?php 
+
+
+class User{
+
+private $name;
+private $surname;
+private $email;
+private $password;
+private $isRegistered;
+private CreditCard $creditCard;
+private $cart = [];
+
+function __construct($_name , $_surname , $_email , $_password , $_isRegistered )
+{
+    $this->setName($_name);
+    $this->setSurname($_surname);
+    $this->setEmail($_email);
+    $this->setPassword($_password);
+    $this->setIsRegistered($_isRegistered);
+    $this->creditCard = new CreditCard("1234123456785678" , "123" , "11 27");
+}
+
+
+/**
+ * Get the value of name
+ */ 
+public function getName()
+{
+return $this->name;
+}
+
+/**
+ * Set the value of name
+ *
+ * @return  self
+ */ 
+public function setName($name)
+{
+$this->name = $name;
+
+return $this;
+}
+
+/**
+ * Get the value of surname
+ */ 
+public function getSurname()
+{
+return $this->surname;
+}
+
+/**
+ * Set the value of surname
+ *
+ * @return  self
+ */ 
+public function setSurname($surname)
+{
+$this->surname = $surname;
+
+return $this;
+}
+
+/**
+ * Get the value of email
+ */ 
+public function getEmail()
+{
+return $this->email;
+}
+
+/**
+ * Set the value of email
+ *
+ * @return  self
+ */ 
+public function setEmail($email)
+{
+$this->email = $email;
+
+return $this;
+}
+
+/**
+ * Get the value of password
+ */ 
+public function getPassword()
+{
+return $this->password;
+}
+
+/**
+ * Set the value of password
+ *
+ * @return  self
+ */ 
+public function setPassword($password)
+{
+$this->password = $password;
+
+return $this;
+}
+
+/**
+ * Get the value of isRegistered
+ */ 
+public function getIsRegistered()
+{
+return $this->isRegistered;
+}
+
+/**
+ * Set the value of isRegistered
+ *
+ * @return  self
+ */ 
+public function setIsRegistered($isRegistered)
+{
+$this->isRegistered = $isRegistered;
+
+return $this;
+}
+}
