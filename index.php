a3fd2d5475a1e18fd3e06c70c772161165da8f29
Michael Bombini <bombini.michael@gmail.com>
2022-07-15 15:47:41 +0200

    Added try catch working

--- index.php
+++ index.php
@@ -30,10 +30,11 @@ $pallaRimbalzante = new GameProduct
 ("Palla" , "7.99€" , "Palla per cani rimbalzante resistente in gomma naturale" , "Verde" , "Gomma Naturale");
 
 
-$userUno = new User("Mario" , "Rossi" , "mariorossi@gmail.com"  , "MarioRossi111" , true , "1237637384272632" , "100" , "2026");
+$userUno = new User("Mario" , "Rossi" , "mariorossi@gmail.com"  , "MarioRossi111" , true , "1237637384272632" , "100" , "2020");
 
-$userDue = new User("Luigi" , "Verdi" , "luigiverdi@gmail.com"  , "LuigiVerdi222" , false , "6292173483261275" , "166" , "2020");
+$userDue = new User("Luigi" , "Verdi" , "luigiverdi@gmail.com"  , "LuigiVerdi222" , false , "6292173483261275" , "166" , "2023");
 
+$userTre = new User("giacomo" , "gialli" , "giacomogialli@gmail.com"  , "giacomogialli6" , true , "7652747615627422" , "123" , 2020);
 
 $userUno->setCart($crocchetteCani);
 $userUno->setCart($crocchetteGatti);
@@ -46,23 +47,60 @@ $userDue->setCart($pallaRimbalzante);
 
 $userUnoExpire = $userUno->getExpiryDate();
 $userDueExpire = $userDue->getExpiryDate();
+$userTreExpire = $userTre->getExpiryDate();
+
 
 echo "<h1>";
-if(($userUno->isExpired($userUnoExpire))){
-    echo "La carta di userUno è scaduta";
+
+try{
+
+    if(($userUno->isExpired($userUnoExpire))){
+    echo "La carta di userUno non è scaduta";
 }
 else 
-    echo "La carta di userDue non è scaduta";
+echo "La carta di userUno è scaduta";
+
+}
+
+catch(Exception $e){
+    echo "I dati passati non sono di tipo stringa" ;
+}
+
+
 
 echo "<br>";
 
+try{
     if(($userDue->isExpired($userDueExpire))){
-        echo "La carta di userDue è scaduta";
+        echo "La carta di userDue non è scaduta";
         
     }
     else 
-        echo "La carta di userDue non è scaduta";
+    echo "La carta di userDue è scaduta";
     
+}
+catch(Exception $e){
+    echo "I dati passati non sono di tipo stringa" ;
+}
+
+echo "<br>";
+
+try{
+    if(($userTre->isExpired($userTreExpire))){
+        echo "La carta di userTre non è scaduta";
+        
+    }
+    else 
+    echo "La carta di userTre è scaduta";
+    
+}
+catch(Exception $e){
+    echo "I dati passati non sono di tipo stringa" ;
+}
+
+
+
+
 
 echo "</h1>";
 
